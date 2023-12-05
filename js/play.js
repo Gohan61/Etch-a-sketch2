c62c91aba2c9231b987a51f4dced5eef9fe549d7
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2023-12-05 14:49:18 +0100

    Insert new scaling squared grid
    Feat: create and replace a grid with squares by clicking button and
    entering squares per side

--- js/play.js
+++ js/play.js
@@ -1,24 +1,65 @@
+const header = document.createElement("h1");
+let amountOfSquares = 0;
 const containerDiv = document.createElement("div");
 let squareDivs = [];
+const popupButton = document.createElement("button");
 
-for (let i = 0; i < 256; i++) {
-  squareDivs = document.createElement("div");
-  containerDiv.appendChild(squareDivs);
-  squareDivs.setAttribute("class", "innerDiv");
+//Grid elements + interaction
+function createDivs(amountOfSquares) {
+  while (containerDiv.firstChild) {
+    containerDiv.removeChild(containerDiv.lastChild);
+  }
+  for (let i = 0; i < amountOfSquares; i++) {
+    squareDivs = document.createElement("div");
+    containerDiv.appendChild(squareDivs);
+    squareDivs.setAttribute("class", "innerDiv flexBasis");
+  }
+  containerDiv.setAttribute("class", "container");
+  document.body.appendChild(containerDiv);
+
+  const setFlexBasis = document.querySelectorAll(".flexBasis");
+  setFlexBasis.forEach((el) =>
+    el.setAttribute(
+      "style",
+      `display: flex; flex-grow: 1; flex-shrink: 0; flex-basis: calc(100% / ${Math.pow(
+        amountOfSquares,
+        1 / 2
+      )})`
+    )
+  );
+
+  const hoveredDiv = document.querySelectorAll(".innerDiv");
+  hoveredDiv.forEach((el) =>
+    el.addEventListener("mouseover", function color(coloredDiv) {
+      coloredDiv.currentTarget.style.backgroundColor = "blue";
+    })
+  );
+
+  hoveredDiv.forEach((el) =>
+    el.addEventListener("mouseout", function noncolor(whiteDiv) {
+      whiteDiv.currentTarget.style.backgroundColor = "";
+    })
+  );
 }
 
-containerDiv.setAttribute("class", "container");
-document.body.appendChild(containerDiv);
+createDivs(2 * 2);
 
-const hoveredDiv = document.querySelectorAll(".innerDiv");
-hoveredDiv.forEach((el) =>
-  el.addEventListener("mouseover", function color(coloredDiv) {
-    coloredDiv.currentTarget.style.backgroundColor = "blue";
-  })
-);
+//Header styling
+header.textContent = "Etch-a-sketch";
+header.setAttribute("style", "color: blue");
+document.body.prepend(header);
 
-hoveredDiv.forEach((el) =>
-  el.addEventListener("mouseout", function noncolor(whiteDiv) {
-    whiteDiv.currentTarget.style.backgroundColor = "";
-  })
-);
+//Button interaction
+popupButton.textContent = "Press me";
+popupButton.addEventListener("click", function askUser() {
+  amountOfSquares = prompt(
+    "Enter a number for the amount of squares in the grid",
+    0
+  );
+  if (amountOfSquares > 100) {
+    alert("Please enter a number below 101");
+  } else {
+    createDivs(amountOfSquares * amountOfSquares);
+  }
+});
+document.body.insertBefore(popupButton, containerDiv);
